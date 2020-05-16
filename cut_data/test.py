3c70af5c52ec90899f5c7b61a8fab92c1ba99a09
jhk1m <54781888+jhk1m@users.noreply.github.com>
2020-05-16 19:58:58 -0400

    Add files via upload

---
+++ cut_data/test.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+x = np.load("X_test.npy")
+y = np.load("y_test.npy", allow_pickle='False')
+
+aa = 1
---
+++ utils/constants.py
@@ -0,0 +1,16 @@
+
+TRAIN_FILES = ['../cut_data/'
+               ]
+
+TEST_FILES = ['../cut_data/'
+              ]
+
+MAX_NB_VARIABLES = [2
+                    ]
+
+MAX_TIMESTEPS_LIST = [506
+                      ]
+
+
+NB_CLASSES_LIST = [7
+                   ]
